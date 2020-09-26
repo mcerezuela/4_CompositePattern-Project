22888071f16c6d7003bd91a5a50eb8cbb149ffd6
marcusalten <marcus.cerezuela@alten.es>
2020-09-26 17:35:00 +0200

    Added class structure

---
+++ CompositePattern/test/com/smashtik/patterns/compositepattern/HtmlElementTest.java
@@ -0,0 +1,38 @@
+package com.smashtik.patterns.compositepattern;
+
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HtmlElementTest {
+
+    @BeforeEach
+    void setUp() {
+    }
+
+    @AfterEach
+    void tearDown() {
+    }
+
+    @Test
+    void getTagName() {
+    }
+
+    @Test
+    void setStartTag() {
+    }
+
+    @Test
+    void setEndTag() {
+    }
+
+    @Test
+    void setTagBody() {
+    }
+
+    @Test
+    void generateHtml() {
+    }
+}
---
+++ CompositePattern/test/com/smashtik/patterns/compositepattern/HtmlParentElementTest.java
@@ -0,0 +1,42 @@
+package com.smashtik.patterns.compositepattern;
+
+import static org.junit.jupiter.api.Assertions.*;
+
+class HtmlParentElementTest {
+
+    @org.junit.jupiter.api.BeforeEach
+    void setUp() {
+    }
+
+    @org.junit.jupiter.api.AfterEach
+    void tearDown() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getTagName() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setStartTag() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void setEndTag() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void addChildTag() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void removeChildTag() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void getChildren() {
+    }
+
+    @org.junit.jupiter.api.Test
+    void generateHtml() {
+    }
+}
